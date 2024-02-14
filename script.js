61b9a4b8a4fdb800f18b9c85aebdc7c410514e34
jefersongsg <jefersonsilva5525@gmail.com>
2024-02-14 13:59:36 -0300

    alterando valor do input

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-const convertButton = document.querySelector(".convert-button")
-
+    const convertButton = document.querySelector(".convert-button")
+ 
 function convertValues(){
     const inputCurrencyValue = document.querySelector(".input-currency").value 
+    const currencyValueToConvert = document.querySelector(".to-convert-currency-value") //valor em Real
+    const currencyValueToConverted = document.querySelector(".currency-value") // outros moedas
 
-    console.log(inputCurrencyValue)
+    const dolarToday = 5.2
+
+    const convertedValue = inputCurrencyValue / dolarToday
+
+    currencyValueToConverted.innerHTML = convertedValue
+    currencyValueToConvert.innerHTML = inputCurrencyValue
+  
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR",{
+        style:"currency",
+        currency:"BRL"
+    }).format(inputCurrencyValue)
+
+
+    currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US",{
+        style:"currency",
+        currency:"USD"
+    }).format(convertedValue)
+
+
+
+
+
 }
-
 
 
 convertButton.addEventListener("click", convertValues)
-
-
-
-
-
-/*
-
-
-
- {
-   
-}
-
-
-*/
